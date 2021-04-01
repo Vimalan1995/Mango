ddf3ee6d3112af8c2bf9ba617a44d0461f3be04c
Vimalan <vimalan.karuna@gmail.com>
2021-04-01 14:20:07 -0400

    hash encryption

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const ejs = require("ejs");
 
 
@@ -25,7 +26,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret:secret , encryptedFields:["secret"]});
+// userSchema.plugin(encrypt, {secret:secret , encryptedFields:["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -40,7 +41,7 @@ app.get("/login",function(req,res){
 
 app.post("/login",function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err,foundUser){
     if(err){
@@ -63,7 +64,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
